74e6fa9cb4e70fb85745061d28b1382479e7a9cb
Matthew Knox <mrkno@users.noreply.github.com>
2016-12-28 15:38:21 +1300

    Update insult.js

--- insult.js
+++ insult.js
@@ -1,4 +1,4 @@
-var reddit = require.once(rootPathJoin('core/common/reddit.js')),
+var reddit = require.once('concierge/reddit'),
     results = [];
 
 exports.insult = function(callback, waitCallback) {
